80db2b1341c0de4712df8dd1905268dbdb1d0d2b
Michael Pozhidaev <msp@luwrain.org>
2026-03-26 13:31:00 +0700

    docker utilities

---
+++ app-linux-term/src/test/java/org/luwrain/app/linux_term/TermInfoTest.java
@@ -0,0 +1,14 @@
+
+package org.luwrain.app.linux_term;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TermInfoTest
+{
+    @Test void load()
+    {
+	final var t = new TermInfo();
+	//	assertTrue(false);
+    }
+}
---
+++ linux/src/main/java/org/luwrain/docker/AbstractSyncRunner.java
@@ -0,0 +1,10 @@
+
+package org.luwrain.linux.docker;
+
+import java.util.*;
+
+public interface AbstractSyncRunner
+{
+    List<String> run(String[] args);
+}
+
